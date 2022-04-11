77404d4cc0da9d31c86e0819adef529b592f524e
aezrath96 <100738736+aezrath96@users.noreply.github.com>
2022-04-11 22:56:47 +0200

    Changed .env description

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const tmi = require("tmi.js");
 var mysql = require("mysql");
 const fs = require("fs");
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
